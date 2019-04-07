0d0ec03adc91a95c26d50e8ff0e0bcffd8e2fef1
Davi <chalitameneguelli14@gmail.com>
2019-04-06 21:46:35 -0300

    index do twitter em construcao

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+const loadTemplates = (parent) => {
+    const items = parent.querySelectorAll('[template]');
+
+    items.forEach(item => {
+
+        const template = item.getAttribute('template');
+
+        fetch(template)
+
+            .then((response) => {
+                return response.text();
+            })
+            .then(content => {
+                item.innerHTML = content;
+
+                loadTemplates(item);
+            })
+    });
+}
+
 const onWindowsLoaded = () => {
     console.log('windows loaded');
 }
