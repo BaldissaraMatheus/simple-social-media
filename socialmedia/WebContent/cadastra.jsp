f7d2629c6a2af85b57e37debf2903d0ba6fa27be
Matheus Baldissara <baldissaramatheus@gmail.com>
2019-05-05 22:53:59 -0300

    Criada arquitetura básica do projeto java

---
+++ socialmedia/WebContent/cadastra.jsp
@@ -0,0 +1,11 @@
+<%
+
+%>
+
+<html>
+<head>
+ <link rel="stylesheet" type="text/css" href="assets/stylesheets/foo.css"> 
+</head>
+Usuário ${param.nome} cadastrado com sucesso!
+
+</html>
---
+++ socialmedia/src/br/com/uff/socialmedia/controller/CadastraServlet.java
@@ -0,0 +1,36 @@
+package br.com.uff.socialmedia.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+import br.com.uff.socialmedia.model.User;
+import br.com.uff.socialmedia.model.dao.Dao;
+
+@WebServlet(urlPatterns="/cadastra")
+public class CadastraServlet extends HttpServlet{
+	
+	private static Dao<User> UserDao;
+	
+	User usuario = new User();
+	
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws IOException, ServletException {
+
+		String nome = req.getParameter("nome");		
+		usuario.setNome(nome);
+		
+		UserDao.save(usuario);
+		
+	    RequestDispatcher rd = req.getRequestDispatcher("/cadastra.jsp");
+	    req.setAttribute("nome", UserDao.get(0).getNome());
+	    rd.forward(req, res);
+		
+	}
+	
+}
