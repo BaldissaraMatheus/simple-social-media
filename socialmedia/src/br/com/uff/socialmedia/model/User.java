55341dcd8c5d38efc9b704569e6db7d1b13c09a1
Matheus Baldissara <baldissaramatheus@gmail.com>
2019-06-08 16:16:54 -0300

    Inclui campo password na classe usuario

--- socialmedia/src/br/com/uff/socialmedia/model/User.java
+++ socialmedia/src/br/com/uff/socialmedia/model/User.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class User {
 	private String username;
 	private String email;
+	private String password;
 	private List<Post> posts;
 	private String name;
 	private Icon icon;
@@ -26,6 +27,14 @@ public class User {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	public String getName() {
